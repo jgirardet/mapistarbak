44a19763c47bce214d46949d6ed122467b46ad04
Jimmy Girardet <test@ma.fr>
2018-02-05 18:12:44 +0100

    initial

---
+++ tests/test_mapistar.py
@@ -0,0 +1,42 @@
+    # -*- coding: utf-8 -*-
+
+"""
+test_mapistar
+----------------------------------
+Tests for `mapistar` module.
+"""
+
+# Third Party Libraries
+from apistar.test import TestClient
+from app import app
+from pseudos.models import Pseudo
+
+
+
+# mapistar
+from mapistar import mapistar
+
+
+def test_mapistar():
+    assert mapistar.main() == "hello"
+
+
+
+
+def test_http_request():
+    """
+    Testing a view, using the test client with
+    """
+    client = TestClient(app)
+    response = client.get('http://localhost/pseudos/')
+    assert response.status_code == 200
+
+def test_http_request_with_fixture(ss):
+    """
+    Testing a view, using the test client with
+    but testing with fixture
+    """
+    client = TestClient(app)
+    e = ss.Pseudo.objects.all()
+    response = client.get('http://localhost/pseudos/')
+    assert response.status_code == 200
