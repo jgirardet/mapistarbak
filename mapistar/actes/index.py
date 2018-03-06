c7182aee9f8c188fef01f5b88bb0c3bcc9f48038
jgirardet <moi@moi>
2018-03-06 22:18:10 +0100

    index

--- mapistar/actes/index.py
+++ mapistar/actes/index.py
@@ -31,33 +31,41 @@ class ActeViews:
         # self.create.__name__ = 'create_obs'
 
     def create(self):
-        def create_acte(obj: actes_schemas[self.model_name].creater, auth: Auth,
-                        db: DB) -> Response:
+        def create_acte(obj: actes_schemas[self.model_name].creater,
+                        auth: Auth, db: DB) -> Response:
             patient = get_or_404(db.Patient, id=obj.pop('patient_id'))
-            obj = self.model.objects.create(patient=patient, owner=auth.user, **obj)
-            return Response(actes_schemas[self.model_name].getter(obj), status=201)
+            obj = self.model.objects.create(
+                patient=patient, owner=auth.user, **obj)
+            return Response(
+                actes_schemas[self.model_name].getter(obj), status=201)
 
         create_acte.__name__ = "create_" + self.name
         create_acte.__doc__ = f""" Create a new {self.name}"""
         return create_acte
 
     def liste(self):
-        def liste_acte(patient_id: int) -> List[actes_schemas[self.model_name].getter]:
+        def liste_acte(patient_id: int
+                       ) -> List[actes_schemas[self.model_name].getter]:
             # model = get_model(type_acte, db)
             print(patient_id)
-            objs = self.model.objects.filter(patient_id=patient_id).order_by('-created')
+            objs = self.model.objects.filter(
+                patient_id=patient_id).order_by('-created')
 
-            return [actes_schemas[self.model_name].getter(item) for item in objs]
+            return [
+                actes_schemas[self.model_name].getter(item) for item in objs
+            ]
 
         liste_acte.__name__ = "liste_" + self.name
         liste_acte.__doc__ = f"""Liste all {self.name} of given patient"""
         return liste_acte
 
     def update(self):
-        @annotate(
-            permissions=[IsAuthenticated(),
-                         ActesWritePermission(models=self.__class__.instances)])
-        def acte_update(obj_id: int, new_data: actes_schemas[self.model_name].updater,
+        @annotate(permissions=[
+            IsAuthenticated(),
+            ActesWritePermission(models=self.__class__.instances)
+        ])
+        def acte_update(obj_id: int,
+                        new_data: actes_schemas[self.model_name].updater,
                         auth: Auth) -> Response:
 
             # check against empty data
@@ -71,16 +79,18 @@ class ActeViews:
             except AttributeError as e:
                 # request should be for valide fields
                 raise BadRequest from e
-            return Response(actes_schemas[self.model_name].getter(obj), status=201)
+            return Response(
+                actes_schemas[self.model_name].getter(obj), status=201)
 
         acte_update.__name__ = "update_" + self.name
         acte_update.__doc__ = f""" Update  {self.name}"""
         return acte_update
 
     def delete(self):
-        @annotate(
-            permissions=[IsAuthenticated(),
-                         ActesWritePermission(models=self.__class__.instances)])
+        @annotate(permissions=[
+            IsAuthenticated(),
+            ActesWritePermission(models=self.__class__.instances)
+        ])
         def acte_delete(obj_id: int, auth: Auth) -> Response:
             obj = self.model.objects.get(id=obj_id)
             obj.delete()
@@ -91,7 +101,8 @@ class ActeViews:
         return acte_delete
 
     def get_one(self):
-        def get_one_acte(obj_id: int, auth: Auth) -> actes_schemas[self.model_name].getter:
+        def get_one_acte(obj_id: int,
+                         auth: Auth) -> actes_schemas[self.model_name].getter:
             obj = get_or_404(self.model, id=obj_id)
             print(obj)
             return actes_schemas[self.model_name].getter(obj)
@@ -102,13 +113,10 @@ class ActeViews:
 
     def urls(self):
         return [
-            Route(
-                '/' + self.name + '/{obj_id}/',
-                'GET',
-                self.get_one(),
-                name=self.name + "_GERGERregerg"),
+            Route('/' + self.name + '/{obj_id}/', 'GET', self.get_one()),
             Route('/' + self.name + '/', 'POST', self.create()),
-            Route('/' + self.name + '/patient/{patient_id}/', 'GET', self.liste()),
+            Route('/' + self.name + '/patient/{patient_id}/', 'GET',
+                  self.liste()),
             Route('/' + self.name + '/{obj_id}/', 'PATCH', self.update()),
             Route('/' + self.name + '/{obj_id}/', 'DELETE', self.delete()),
         ]
